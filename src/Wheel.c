4cbad63b2e3c131f534ce42ea532532f1143f39b
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-11-23 01:53:52 +0800

    Get rid of control delay. Using response checking instead.
    Add WCTRL_TIMEOUT and WCTRL_WRONG_RESPONSE return value definition.
    Modify WCTRL_Control().

--- src/Wheel.c
+++ src/Wheel.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
+#include <time.h>
 #include <libserialport.h>
 
 #include "Wheel.h"
@@ -7,10 +8,10 @@
 #define WCTRL_HEAD_CHAR 'W'
 #define WCTRL_BUFLEN    7
 
+#define WCTRL_RESP_STR	"WOK"
+
 #define WCTRL_MIN_SPEED 0
 #define WCTRL_MAX_SPEED 510
-
-#define WCTRL_WRITE_WATIING 100
 
 int WCTRL_Init(WCTRL* wheelCtrlPtr, const char* deviceName, int baudrate)
 {
@@ -57,11 +58,17 @@ int WCTRL_Init(WCTRL* wheelCtrlPtr, const char* deviceName, int baudrate)
     return WCTRL_NO_ERROR;
 }
 
-int WCTRL_Control(WCTRL wheelCtrl, int leftSpeed, int rightSpeed, int delay)
+int WCTRL_Control(WCTRL wheelCtrl, int leftSpeed, int rightSpeed, int timeout)
 {
     int iResult;
     char ctrlBuf[WCTRL_BUFLEN + 1] = {0};
     struct sp_port* serialPort = (struct sp_port*)wheelCtrl;
+
+	int timeCost;
+	struct timespec timeHold, tmpTime;
+
+	char tmpRead;
+	int respStrLen, counter;
 
     // Check if speed all legal
     if(leftSpeed < WCTRL_MIN_SPEED || leftSpeed > WCTRL_MAX_SPEED)
@@ -76,15 +83,65 @@ int WCTRL_Control(WCTRL wheelCtrl, int leftSpeed, int rightSpeed, int delay)
     // Process Control String
     sprintf(ctrlBuf, "%c%03d%03d", WCTRL_HEAD_CHAR, leftSpeed, rightSpeed);
 
+	// Get current time
+	clock_gettime(CLOCK_MONOTONIC, &timeHold);
+
     // Output Control String
-    iResult = sp_blocking_write(serialPort, ctrlBuf, WCTRL_BUFLEN, WCTRL_WRITE_WATIING);
+    iResult = sp_blocking_write(serialPort, ctrlBuf, WCTRL_BUFLEN, timeout);
     if(iResult < 0)
     {
         return WCTRL_SERIAL_FAILED;
     }
 
-    // Delay
-    usleep(delay * 1000);
+	// Get response
+	respStrLen = strlen(WCTRL_RESP_STR);
+	counter = 0;
+	while(1)
+	{
+		// Checking time cost
+		clock_gettime(CLOCK_MONOTONIC, &tmpTime);
+		tmpTime.tv_sec = tmpTime.tv_sec - timeHold.tv_sec;
+		tmpTime.tv_nsec = tmpTime.tv_nsec - timeHold.tv_nsec;
+		timeCost = tmpTime.tv_sec * 1000 + tmpTime.tv_nsec / 1000;
+		if(timeCost >= timeout)
+		{
+			return WCTRL_TIMEOUT;
+		}
+
+		// Non-blocking reading
+		iResult = sp_nonblocking_read(serialPort, &tmpRead, sizeof(char));
+		if(iResult == 0)
+		{
+			continue;
+		}
+		else if(iResult < 0)
+		{
+			return WCTRL_SERIAL_FAILED;
+		}
+
+		// Check and store received data
+		if(counter == 0 && tmpRead != WCTRL_HEAD_CHAR)
+		{
+			continue;
+		}
+		else
+		{
+			ctrlBuf[counter++] = tmpRead;
+		}
+
+		// Check if finished
+		if(counter == respStrLen)
+		{
+			ctrlBuf[counter] = '\0';
+			break;
+		}
+	}
+
+	// Check response
+	if(strcmp(ctrlBuf, WCTRL_RESP_STR) != 0)
+	{
+		return WCTRL_WRONG_RESPONSE;
+	}
 
     return WCTRL_NO_ERROR;
 }
--- src/Wheel.h
+++ src/Wheel.h
@@ -5,7 +5,9 @@ enum WCTRL_RETURN_VALUE
 {
     WCTRL_NO_ERROR          = 0,
     WCTRL_SERIAL_FAILED     = -1,
-    WCTRL_ILLEGAL_ARGUMENT  = -2
+    WCTRL_ILLEGAL_ARGUMENT  = -2,
+	WCTRL_TIMEOUT			= -3,
+	WCTRL_WRONG_RESPONSE	= -4
 };
 
 typedef void* WCTRL;
@@ -15,7 +17,7 @@ extern "C" {
 #endif // __cplusplus
 
 int WCTRL_Init(WCTRL* wheelCtrlPtr, const char* deviceName, int baudrate);
-int WCTRL_Control(WCTRL wheelCtrl, int leftSpeed, int rightSpeed, int delay);
+int WCTRL_Control(WCTRL wheelCtrl, int leftSpeed, int rightSpeed, int timeout);
 int WCTRL_Close(WCTRL wheelCtrl);
 
 #ifdef __cplusplus
